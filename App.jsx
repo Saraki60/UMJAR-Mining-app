f58962e9960cf38f934fe07db72d1662c59a767b
Saraki60 <jaffarsabiulawan@gmail.com>
2026-05-11 12:22:24 +0300

    Update and rename src/App.jsx to App.jsx

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{
+      background: "black",
+      color: "gold",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "32px",
+      fontWeight: "bold"
+    }}>
+      UMJAR MINING
+    </div>
+  );
+}
