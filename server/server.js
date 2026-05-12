794c29b7b8654c07bce5856ecd13291a0ea169db
Saraki60 <jaffarsabiulawan@gmail.com>
2026-05-12 12:46:31 +0300

    Set up Express server with MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoute = require("./routes/auth");
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
   res.send("UMJAR API Running");
